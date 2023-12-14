0b3d8b9734a9b28355a092f2b380201c7932410c
Torea Patissier <p.torea@icloud.com>
2023-12-14 23:46:10 +0100

    feat : register

--- ressources/personne.sql
+++ ressources/personne.sql
@@ -4,5 +4,13 @@ CREATE TABLE personne (
                           prenom	VARCHAR( 40 )
 );
 
+CREATE TABLE utilisateur (
+                             id        int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                             email     VARCHAR(200) NOT NULL,
+                             password  VARCHAR(255) NOT NULL,
+                             personne_id int,
+                             FOREIGN KEY (personne_id) REFERENCES personne(id)
+);
+
 INSERT INTO personne (nom, prenom)
 VALUES ('Monkey D', 'Luffy'), ('Hancock', 'Boa'), ('Mihawk', 'Dracule'), ('Figarland', 'Shanks');
--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config()
 const app = express();
 const URL = 'http://localhost';
-const PORT = 8082;
+const PORT = process.env.PORT;
 const personneRoutes = require('./src/routes/personne.route');
 const livreGenreRoutes = require('./src/routes/livre_genre.route');
 const livreRoutes = require('./src/routes/livre.route');
 const genreRoutes = require('./src/routes/genre.route');
 const couvertureRoutes = require ('./src/routes/couverture.route');
 const auteurRoutes = require ('./src/routes/auteur.route');
+const authRoutes = require('./src/routes/auth.route');
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Bienvenue sur mon API !');
 });
+app.use('/api/auth', authRoutes);
 app.use('/api/personnes', personneRoutes);
 app.use('/api/livre_genres', livreGenreRoutes);
-app.use('/api/livres',livreRoutes);
-app.use('/api/genres',genreRoutes);
-app.use('/api/couvertures',couvertureRoutes);
-app.use('/api/auteurs',auteurRoutes);
+app.use('/api/livres', livreRoutes);
+app.use('/api/genres', genreRoutes);
+app.use('/api/couvertures', couvertureRoutes);
+app.use('/api/auteurs', auteurRoutes);
 
 app.listen(PORT, () => {
   console.log(`Serveur en écoute sur le port ${PORT}`);
